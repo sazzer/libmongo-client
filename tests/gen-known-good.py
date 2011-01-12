b8581a60ed30bbc17bf1bfa6505b9a89eb376b46
Gergely Nagy <algernon@balabit.hu>
2011-01-12 23:02:30 +0100

    tests: More tests.
    Almost all of the base types are tested now.

--- tests/gen-known-good.py
+++ tests/gen-known-good.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 from bson import BSON
+from datetime import datetime
 
 def bson_build_simple ():
     # bson_empty
@@ -20,6 +21,22 @@ def bson_build_simple ():
 
     # bson_boolean
     b = BSON.encode ({"TRUE": True, "FALSE": False})
+    print (b.__repr__ ())
+
+    # bson_utc_datetime
+    b = BSON.encode ({"date": datetime.utcfromtimestamp (1294860709)})
+    print (b.__repr__ ())
+
+    # bson_null
+    b = BSON.encode ({"null": None})
+    print (b.__repr__ ())
+
+    # bson_int32
+    b = BSON.encode ({"int32": 1984})
+    print (b.__repr__ ())
+
+    # bson_int64
+    b = BSON.encode ({"int64": 9876543210})
     print (b.__repr__ ())
 
 bson_build_simple ()
--- tests/test_bson_build_simple.c
+++ tests/test_bson_build_simple.c
@@ -83,6 +83,58 @@ test_bson_boolean (void)
   return dump_bson (b);
 }
 
+gboolean
+test_bson_utc_datetime (void)
+{
+  bson *b;
+
+  b = bson_new ();
+  if (!bson_append_utc_datetime (b, "date", 1294860709000))
+    return FALSE;
+  bson_finish (b);
+
+  return dump_bson (b);
+}
+
+gboolean
+test_bson_null (void)
+{
+  bson *b;
+
+  b = bson_new ();
+  if (!bson_append_null (b, "null"))
+    return FALSE;
+  bson_finish (b);
+
+  return dump_bson (b);
+}
+
+gboolean
+test_bson_int32 (void)
+{
+  bson *b;
+
+  b = bson_new ();
+  if (!bson_append_int32 (b, "int32", 1984))
+    return FALSE;
+  bson_finish (b);
+
+  return dump_bson (b);
+}
+
+gboolean
+test_bson_int64 (void)
+{
+  bson *b;
+
+  b = bson_new ();
+  if (!bson_append_int64 (b, "int64", 9876543210))
+    return FALSE;
+  bson_finish (b);
+
+  return dump_bson (b);
+}
+
 int
 main (void)
 {
@@ -99,6 +151,18 @@ main (void)
     return -1;
 
   if (!test_bson_boolean ())
+    return -1;
+
+  if (!test_bson_utc_datetime ())
+    return -1;
+
+  if (!test_bson_null ())
+    return -1;
+
+  if (!test_bson_int32 ())
+    return -1;
+
+  if (!test_bson_int64 ())
     return -1;
 
   return 0;
