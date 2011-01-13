1aa76d4a52bff0f9c6a1b4fd162d0efb0616f94d
Gergely Nagy <algernon@balabit.hu>
2011-01-13 20:54:09 +0100

    tests: Added mongo_wire invalid param tests.

--- tests/test_invalid.c
+++ tests/test_invalid.c
@@ -2,8 +2,6 @@
 #include "bson.h"
 #include "mongo_wire.h"
 
-#include "test-generator.h"
-
 #include <glib.h>
 
 void
@@ -105,9 +103,56 @@ test_invalid_bson_cursor ()
   bson_free (b);
 }
 
+void
+test_invalid_mongo_wire (void)
+{
+  bson *sel, *data, *u;
+  mongo_packet *p;
+
+  sel = bson_new ();
+  bson_append_string (sel, "_id", "none", -1);
+  bson_finish (sel);
+
+  data = bson_new ();
+  bson_append_string (data, "name", "nobody", -1);
+  bson_append_int32 (data, "age", 120);
+  bson_finish (data);
+
+  u = bson_new ();
+
+  TEST (mongo_wire_packet_free);
+  mongo_wire_packet_free (NULL);
+  PASS ();
+
+  TEST (mongo_wire_packet_get_parts);
+  g_assert_cmpint (mongo_wire_packet_get_header (NULL, NULL), ==, -1);
+  g_assert_cmpint (mongo_wire_packet_get_data (NULL, NULL), ==, -1);
+  PASS ();
+
+  TEST (mongo_wire_cmd_update);
+  g_assert (mongo_wire_cmd_update (1, NULL, 0, NULL, NULL) == NULL);
+  g_assert (mongo_wire_cmd_update (1, NULL, 0, sel, NULL) == NULL);
+  g_assert (mongo_wire_cmd_update (1, NULL, 0, NULL, data) == NULL);
+  g_assert (mongo_wire_cmd_update (1, NULL, 0, sel, data) == NULL);
+  g_assert (mongo_wire_cmd_update (1, "test.ns", 0, NULL, NULL) == NULL);
+  g_assert (mongo_wire_cmd_update (1, "test.ns", 0, sel, u) == NULL);
+  g_assert ((p = mongo_wire_cmd_update (1, "test.ns", 0, sel, data)));
+  mongo_wire_packet_free (p);
+  PASS ();
+
+  TEST (mongo_wire_cmd_insert);
+  g_assert (mongo_wire_cmd_insert (1, NULL, NULL) == NULL);
+  g_assert (mongo_wire_cmd_insert (1, NULL, data) == NULL);
+  g_assert (mongo_wire_cmd_insert (1, "test.ns", u) == NULL);
+  g_assert ((p = mongo_wire_cmd_insert (1, "test.ns", data)));
+  mongo_wire_packet_free (p);
+  PASS ();
+}
+
 int
 main (void)
 {
   test_invalid_bson_null ();
   test_invalid_bson_cursor ();
+  test_invalid_mongo_wire ();
 }
