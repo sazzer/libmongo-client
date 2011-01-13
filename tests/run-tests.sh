86d3db9293711b6ead18ebb2f4b46c9a267d0a9c
Gergely Nagy <algernon@balabit.hu>
2011-01-13 10:16:28 +0100

    tests: Transition to python-based testing
    Instead of generating a known-good output and comparing against that
    (which is vulnerable, due to the order of keys being unimportant in
    most cases, yet, can vary), we parse the output of our test programs
    with python, and compare the decoded representations instead.
    
    This has the downside of requiring PyMongo >= 1.9 for the test suite,
    but I can live with that.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,14 +4,8 @@ set -e
 run_test ()
 {
 	test=$1
-	t="$test.out"
 
-	echo "# Running ${test}..."
-	if ! ./test_${test} >$t 2>/dev/null; then
-		return 1
-	fi
-	if ! cmp $t ${srcdir}/${test}.ok 2>/dev/null; then
-		diff -u0 $t ${srcdir}/${test}.ok || true
+	if ! ./test_${test} | python ${srcdir}/verify-bson.py ${test}; then
 		return 1
 	fi
 
