12907f128162f8c8158d87ca18644bed7a894d9a
Gergely Nagy <algernon@balabit.hu>
2011-01-15 23:06:02 +0100

    tests: Added a test case to test the mongo_wire_packet setters.

--- tests/test_mongo_wire.c
+++ tests/test_mongo_wire.c
@@ -173,12 +173,84 @@ test_mongo_wire_query ()
   PASS ();
 }
 
+void
+test_mongo_wire_setters (void)
+{
+  bson *n, *f;
+  mongo_packet *p;
+
+  const mongo_packet_header *rhdr;
+  const guint8 *rdata;
+  gint32 hdr_size, data_size, pos;
+
+  mongo_packet_header whdr;
+  guint8 *wdata;
+
+  bson_cursor *c;
+
+  TEST (mongo_wire.setters);
+
+  n = test_bson_generate_nested ();
+  f = test_bson_generate_flat ();
+
+  g_assert ((p = mongo_wire_cmd_insert (1, "test.libmongo", n)));
+  bson_free (n);
+
+  g_assert_cmpint ((hdr_size = mongo_wire_packet_get_header (p, &rhdr)), !=, -1);
+  g_assert_cmpint ((data_size = mongo_wire_packet_get_data (p, &rdata)), !=, -1);
+
+  g_assert_cmpint (rhdr->length, ==,
+		   sizeof (mongo_packet_header) + data_size);
+  g_assert_cmpint (rhdr->id, ==, 1);
+  g_assert_cmpint (rhdr->resp_to, ==, 0);
+
+  memcpy (&whdr, rhdr, sizeof (mongo_packet_header));
+  g_assert ((wdata = g_try_new0 (guint8, data_size)) != NULL);
+
+  whdr.id = 2;
+  whdr.length = 0;
+
+  g_assert (mongo_wire_packet_set_header (p, &whdr));
+  g_assert_cmpint ((hdr_size = mongo_wire_packet_get_header (p, &rhdr)), !=, -1);
+  g_assert_cmpint (rhdr->length, ==, 0);
+  g_assert_cmpint (rhdr->id, ==, 2);
+
+  pos = sizeof (gint32) + strlen ("test.libmongo") + 1;
+  memcpy (wdata, rdata, pos);
+  memcpy (wdata + pos, bson_data (f), bson_size (f));
+
+  g_assert (mongo_wire_packet_set_data (p, wdata, pos + bson_size (f)));
+  bson_free (f);
+  g_assert_cmpint ((hdr_size = mongo_wire_packet_get_header (p, &rhdr)), !=, -1);
+
+  g_assert_cmpint (rhdr->length, !=,
+		   sizeof (mongo_packet_header) + data_size);
+  g_assert_cmpint (rhdr->length, !=, 0);
+
+  g_assert_cmpint ((data_size = mongo_wire_packet_get_data (p, &rdata)), !=, -1);
+  g_assert ((f = bson_new_from_data (rdata + pos, (gint32)rdata[pos] - 1)));
+  bson_finish (f);
+
+  g_assert ((c = bson_find (f, "null")));
+  g_assert_cmpint (bson_cursor_type (c), ==, BSON_TYPE_NULL);
+  g_assert (bson_cursor_next (c));
+  g_assert_cmpint (bson_cursor_type (c), ==, BSON_TYPE_BOOLEAN);
+
+  g_free (c);
+  bson_free (f);
+
+  g_free (wdata);
+
+  PASS ();
+}
+
 int
 main (void)
 {
   test_mongo_wire_update ();
   test_mongo_wire_insert ();
   test_mongo_wire_query ();
+  test_mongo_wire_setters ();
 
   return 0;
 }
