c0e22728e25edf6ae517e1f7ffe8b230da2059b3
Gergely Nagy <algernon@balabit.hu>
2011-03-02 08:17:35 +0100

    tests: Remove refactored tests from test_invalid.

--- tests/test_invalid.c
+++ tests/test_invalid.c
@@ -5,50 +5,6 @@
 #include <glib.h>
 
 void
-test_invalid_bson_null ()
-{
-  bson *b;
-
-  g_assert ((b = bson_new ()));
-
-  TEST (bson_invalid_double_params);
-  g_assert (bson_append_double (b, NULL, 3.14) == FALSE);
-  PASS ();
-
-  TEST (bson_invalid_string_params);
-  g_assert (bson_append_string (b, NULL, "str", -1) == FALSE);
-  g_assert (bson_append_string (b, "test", NULL, -1) == FALSE);
-  g_assert (bson_append_string (b, "test", NULL, 0) == FALSE);
-  g_assert (bson_append_string (b, "test", "string", 0) == FALSE);
-  PASS ();
-
-  TEST (bson_invalid_document_params);
-  g_assert (bson_append_document (b, NULL, NULL) == FALSE);
-  g_assert (bson_append_document (b, "test", b) == FALSE);
-  PASS ();
-
-  TEST (bson_invalid_oid_params);
-  g_assert (bson_append_oid (b, NULL, NULL) == FALSE);
-  g_assert (bson_append_oid (b, "_id", NULL) == FALSE);
-  PASS ();
-
-  TEST (bson_invalid_regex_params);
-  g_assert (bson_append_regex (b, NULL, NULL, NULL) == FALSE);
-  g_assert (bson_append_regex (b, "regex", NULL, NULL) == FALSE);
-  g_assert (bson_append_regex (b, "regex", "/1234/", NULL) == FALSE);
-  g_assert (bson_append_regex (b, "regex", NULL, "i") == FALSE);
-  PASS ();
-
-  bson_free (b);
-
-  TEST (bson_invalid_from_data);
-  g_assert ((b = bson_new_from_data (NULL, 0)) == NULL);
-  g_assert ((b = bson_new_from_data ((guint8 *)"data", 0)) == NULL);
-  g_assert ((b = bson_new_from_data ((guint8 *)"data", -1)) == NULL);
-  PASS ();
-}
-
-void
 test_invalid_bson_cursor ()
 {
   bson *b;
@@ -156,7 +112,6 @@ main (void)
 {
   PLAN (1, 18);
 
-  test_invalid_bson_null ();
   test_invalid_bson_cursor ();
   test_invalid_mongo_wire ();
 
