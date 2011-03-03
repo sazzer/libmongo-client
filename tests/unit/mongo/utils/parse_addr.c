830fa0ee9c839a7f1be4a0e3eee00d8e31f8ac7e
Gergely Nagy <algernon@balabit.hu>
2011-03-03 18:48:39 +0100

    tests: Add mongo_util_parse_addr() unit test.

--- tests/unit/mongo/utils/parse_addr.c
+++ tests/unit/mongo/utils/parse_addr.c
@@ -2,9 +2,72 @@
 #include "test.h"
 #include "mongo-utils.h"
 
+#include <string.h>
+
 void
 test_mongo_utils_parse_addr (void)
 {
+  gchar *host = "deadbeef";
+  gint port = 42;
+
+  ok (mongo_util_parse_addr (NULL, &host, &port) == FALSE,
+      "mongo_util_parse_addr() fails with a NULL address");
+  is (host, NULL,
+      "Failed parsing sets host to NULL");
+  cmp_ok (port, "==", -1,
+	  "Failed parsing sets port to -1");
+  host = "deadbeef";
+  port = 42;
+
+  ok (mongo_util_parse_addr ("127.0.0.1:27017", &host, NULL) == FALSE,
+      "mongo_util_parse_addr() fails when port is NULL");
+  is (host, NULL,
+      "Failed parsing sets host to NULL");
+  host = "deadbeef";
+  port = 42;
+
+  ok (mongo_util_parse_addr ("127.0.0.1:27017", NULL, &port) == FALSE,
+      "mongo_util_parse_addr() fails when host is NULL");
+  cmp_ok (port, "==", -1,
+	  "Failed parsing sets port to -1");
+  host = "deadbeef";
+  port = 42;
+
+  ok (mongo_util_parse_addr ("127.0.0.1", &host, &port),
+      "mongo_util_parse_addr() works when no port is specified");
+  is (host, "127.0.0.1",
+      "Host parsed successfully");
+  cmp_ok (port, "==", 42,
+	  "Port has been left alone");
+  g_free (host);
+  host = "deadbeef";
+
+  ok (mongo_util_parse_addr ("127.0.0.1:27017", &host, &port),
+      "mongo_util_parse_addr() can parse HOST:PORT pairs");
+  is (host, "127.0.0.1",
+      "Host parsed successfully");
+  cmp_ok (port, "==", 27017,
+	  "Port parsed successfully");
+  g_free (host);
+  host = "deadbeef";
+  port = 42;
+
+  ok (mongo_util_parse_addr ("::1:27017", &host, &port),
+      "mongo_util_parse_addr() can deal with IPv6 addresses");
+  is (host, "::1",
+      "Host parsed successfully");
+  cmp_ok (port, "==", 27017,
+	  "Port parsed successfully");
+  g_free (host);
+  host = "deadbeef";
+  port = 42;
+
+  ok (mongo_util_parse_addr (":27017", &host, &port) == FALSE,
+      "mongo_util_parse_addr() should fail when no host is specified");
+  is (host, NULL,
+      "Failed parsing sets host to NULL");
+  cmp_ok (port, "==", -1,
+	  "Failed parsing sets port to -1");
 }
 
-RUN_TEST (0, mongo_utils_parse_addr);
+RUN_TEST (19, mongo_utils_parse_addr);
