aef7a4a2061ddf04cb3a857df7d5ceb1a4c91789
Gergely Nagy <algernon@balabit.hu>
2011-03-03 18:35:11 +0100

    tests: First batch of mongo-utils unit tests.
    Add unit tests for mongo_utils_oid_init(), mongo_utils_oid_new() and
    mongo_utils_oid_new_with_time().

--- tests/unit/mongo/utils/oid_new_with_time.c
+++ tests/unit/mongo/utils/oid_new_with_time.c
@@ -2,9 +2,38 @@
 #include "test.h"
 #include "mongo-utils.h"
 
+#include <string.h>
+
 void
 test_mongo_utils_oid_new_with_time (void)
 {
+  guint8 *oid1, *oid2, *oid3;
+
+  ok (mongo_util_oid_new_with_time (0, 0) == NULL,
+      "mongo_util_oid_new_with_time() should fail before mongo_util_oid_init()");
+
+  mongo_util_oid_init (0);
+  ok ((oid1 = mongo_util_oid_new_with_time (0, 1)) != NULL,
+      "mongo_util_oid_new_with_time() works");
+  cmp_ok (oid1[11], "==", 1,
+	  "mongo_util_oid_new_with_time() returns an OID with the currect seq ID");
+
+  oid2 = mongo_util_oid_new_with_time (0, 2);
+  oid3 = mongo_util_oid_new_with_time (0, 2);
+
+  ok (memcmp (oid2, oid1, 12) > 0,
+      "OIDs with higher sequence ID sort higher");
+  ok (memcmp (oid2, oid3, 12) == 0,
+      "OIDs with the same sequence ID are equal (within a second)");
+  g_free (oid2);
+  g_free (oid3);
+
+  oid2 = mongo_util_oid_new_with_time (1, 0);
+
+  ok (memcmp (oid2, oid1, 12) > 0,
+      "OIDs with the same sequence ID, a few seconds later sort higher");
+  g_free (oid2);
+  g_free (oid1);
 }
 
-RUN_TEST (0, mongo_utils_oid_new_with_time);
+RUN_TEST (6, mongo_utils_oid_new_with_time);
