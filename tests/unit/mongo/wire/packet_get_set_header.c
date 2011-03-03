d69f2aec60257e83e2463b0c13065761192cee55
Gergely Nagy <algernon@balabit.hu>
2011-03-03 21:30:19 +0100

    tests: Add a few mongo_wire_packet unit test skeletons.

---
+++ tests/unit/mongo/wire/packet_get_set_header.c
@@ -0,0 +1,12 @@
+#include "tap.h"
+#include "test.h"
+#include "mongo-wire.h"
+
+#include <string.h>
+
+void
+test_mongo_wire_packet_get_set_header (void)
+{
+}
+
+RUN_TEST (0, mongo_wire_packet_get_set_header);
---
+++ tests/unit/mongo/wire/packet_new.c
@@ -0,0 +1,12 @@
+#include "tap.h"
+#include "test.h"
+#include "mongo-wire.h"
+
+#include <string.h>
+
+void
+test_mongo_wire_packet_new (void)
+{
+}
+
+RUN_TEST (0, mongo_wire_packet_new);
