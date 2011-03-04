986f2788faeeb1c6fc91d68cfe05d4500b460406
Gergely Nagy <algernon@balabit.hu>
2011-03-04 08:32:41 +0100

    tests: Implement some mongo_wire_reply_packet_get_header() unit tests.
    Implement a few unit tests for mongo_wire_reply_packet_get_headers(),
    that test the error cases.
    
    We need a way to create a reply packet, the libtap library should have
    such a function, along the lines of test_bson_generate_full().

--- tests/unit/mongo/wire/reply_packet_get_header.c
+++ tests/unit/mongo/wire/reply_packet_get_header.c
@@ -2,9 +2,31 @@
 #include "tap.h"
 #include "mongo-wire.h"
 
+#include <string.h>
+
 void
 test_mongo_wire_reply_packet_get_header (void)
 {
+  mongo_packet *p;
+  mongo_packet_header h;
+  mongo_reply_packet_header rh;
+
+  p = mongo_wire_packet_new ();
+  memset (&h, 0, sizeof (mongo_packet_header));
+  h.opcode = 1;
+
+  mongo_wire_packet_set_header (p, &h);
+
+  ok (mongo_wire_reply_packet_get_header (NULL, &rh) == FALSE,
+      "mongo_wire_reply_packet_get_header() fails with a NULL packet");
+  ok (mongo_wire_reply_packet_get_header (p, NULL) == FALSE,
+      "mongo_wire_reply_packet_get_header() fails with a NULL header");
+
+  ok (mongo_wire_reply_packet_get_header (p, &rh) == FALSE,
+      "mongo_wire_reply_packet_get_header() fails if the packet has "
+      "no reply header");
+
+  mongo_wire_packet_free (p);
 }
 
-RUN_TEST (0, mongo_wire_reply_packet_get_header);
+RUN_TEST (3, mongo_wire_reply_packet_get_header);
