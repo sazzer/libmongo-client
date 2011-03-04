4d7f1e5b7bab3c99e274ac6acf30cb9bd16278d3
Gergely Nagy <algernon@balabit.hu>
2011-03-04 21:51:36 +0100

    tests: Implement mongo_wire_reply_packet_get_nth_document() tests.

--- tests/unit/mongo/wire/reply_packet_get_nth_document.c
+++ tests/unit/mongo/wire/reply_packet_get_nth_document.c
@@ -1,10 +1,45 @@
 #include "test.h"
 #include "tap.h"
 #include "mongo-wire.h"
+#include "bson.h"
+
+#include <string.h>
 
 void
 test_mongo_wire_reply_packet_get_nth_document (void)
 {
+  mongo_packet *p;
+  bson *doc;
+  mongo_packet_header h;
+
+  p = mongo_wire_packet_new ();
+  memset (&h, 0, sizeof (mongo_packet_header));
+  h.opcode = 2;
+  h.length = sizeof (mongo_packet_header);
+  mongo_wire_packet_set_header (p, &h);
+
+  ok (mongo_wire_reply_packet_get_nth_document (NULL, 1, &doc) == FALSE,
+      "mongo_wire_reply_packet_get_nth_document() fails with a NULL packet");
+  ok (mongo_wire_reply_packet_get_nth_document (p, 0, &doc) == FALSE,
+      "mongo_wire_reply_packet_get_nth_document() fails with n = 0");
+  ok (mongo_wire_reply_packet_get_nth_document (p, -42, &doc) == FALSE,
+      "mongo_wire_reply_packet_get_nth_document() fails with n < 0");
+  ok (mongo_wire_reply_packet_get_nth_document (p, 1, NULL) == FALSE,
+      "mongo_wire_reply_packet_get_nth_document() fails with a NULL "
+      "destination");
+
+  ok (mongo_wire_reply_packet_get_nth_document (p, 1, &doc) == FALSE,
+      "mongo_wire_reply_packet_get_nth_document() fails with a "
+      "non-reply packet");
+
+  h.opcode = 1;
+  mongo_wire_packet_set_header (p, &h);
+
+  ok (mongo_wire_reply_packet_get_nth_document (p, 1, &doc) == FALSE,
+      "mongo_wire_reply_packet_get_nth_document() fails with an "
+      "incomplete reply packet");
+
+  mongo_wire_packet_free (p);
 }
 
-RUN_TEST (0, mongo_wire_reply_packet_get_nth_document);
+RUN_TEST (6, mongo_wire_reply_packet_get_nth_document);
