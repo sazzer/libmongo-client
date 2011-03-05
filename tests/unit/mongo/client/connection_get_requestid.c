de24fbd2eaa2f23fb3675bb190aca28a3404bf23
Gergely Nagy <algernon@balabit.hu>
2011-03-05 16:02:43 +0100

    tests: Added mongo_connection_get_requestid() unit test.

--- tests/unit/mongo/client/connection_get_requestid.c
+++ tests/unit/mongo/client/connection_get_requestid.c
@@ -5,6 +5,8 @@
 void
 test_mongo_connection_get_requestid (void)
 {
+  ok (mongo_connection_get_requestid (NULL) == -1,
+      "mongo_connection_get_requestid fails with a NULL connection");
 }
 
-RUN_TEST (0, mongo_connection_get_requestid);
+RUN_TEST (1, mongo_connection_get_requestid);
