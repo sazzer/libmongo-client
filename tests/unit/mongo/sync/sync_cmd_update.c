d36cf85c35061940a92aa15a1aa1318db71810bf
Gergely Nagy <algernon@balabit.hu>
2011-03-06 19:23:05 +0100

    tests: Add networked unit tests to some of the mongo_sync commands.
    The mongo_sync_connect(), mongo_sync_reconnect(),
    mongo_sync_cmd_insert(), mongo_sync_cmd_update() and
    mongo_sync_cmd_query() unit tests now have optional parts that require
    a network.
    
    These tests test some basic assumptions about the commands, and the
    librarys reaction to certain events (such as the loss of network).

--- tests/unit/mongo/sync/sync_cmd_update.c
+++ tests/unit/mongo/sync/sync_cmd_update.c
@@ -1,35 +1,82 @@
 #include "test.h"
 #include "mongo.h"
 
+#include "libmongo-private.h"
+
+#include <sys/socket.h>
+
 void
 test_mongo_sync_cmd_update (void)
 {
   mongo_sync_connection *c;
   bson *sel, *upd;
 
-  test_env_setup ();
-
   sel = test_bson_generate_full ();
   upd = test_bson_generate_full ();
   c = test_make_fake_sync_conn (-1, FALSE);
 
-  ok (mongo_sync_cmd_update (NULL, config.ns, 0, sel, upd) == FALSE,
+  ok (mongo_sync_cmd_update (NULL, "test.ns", 0, sel, upd) == FALSE,
       "mongo_sync_cmd_update() fails with a NULL connection");
   ok (mongo_sync_cmd_update (c, NULL, 0, sel, upd) == FALSE,
       "mongo_sync_cmd_update() fails with a NULL namespace");
-  ok (mongo_sync_cmd_update (c, config.ns, 0, NULL, upd) == FALSE,
+  ok (mongo_sync_cmd_update (c, "test.ns", 0, NULL, upd) == FALSE,
       "mongo_sync_cmd_update() fails with a NULL selector");
-  ok (mongo_sync_cmd_update (c, config.ns, 0, sel, NULL) == FALSE,
+  ok (mongo_sync_cmd_update (c, "test.ns", 0, sel, NULL) == FALSE,
       "mongo_sync_cmd_update() fails with a NULL update");
 
-  ok (mongo_sync_cmd_update (c, config.ns, 0, sel, upd) == FALSE,
+  ok (mongo_sync_cmd_update (c, "test.ns", 0, sel, upd) == FALSE,
       "mongo_sync_cmd_update() fails with a bogus FD");
 
   mongo_sync_disconnect (c);
   bson_free (sel);
   bson_free (upd);
 
-  test_env_free ();
+  begin_network_tests (4);
+
+  sel = bson_new ();
+  bson_finish (sel);
+  upd = test_bson_generate_full ();
+  c = mongo_sync_connect (config.primary_host, config.primary_port,
+			  FALSE);
+
+  ok (mongo_sync_cmd_update (c, config.ns,
+			     MONGO_WIRE_FLAG_UPDATE_UPSERT |
+			     MONGO_WIRE_FLAG_UPDATE_MULTI, sel,
+			     upd) == TRUE,
+      "mongo_sync_cmd_update() works");
+
+  shutdown (c->super.fd, SHUT_RDWR);
+  sleep (3);
+
+  ok (mongo_sync_cmd_update (c, config.ns,
+			     MONGO_WIRE_FLAG_UPDATE_UPSERT |
+			     MONGO_WIRE_FLAG_UPDATE_MULTI, sel, upd) == TRUE,
+      "mongo_sync_cmd_update() automatically reconnects");
+
+  mongo_sync_disconnect (c);
+
+  /*
+   * Tests involving a secondary
+   */
+  skip (!config.secondary_host, 2,
+	"Secondary host not set up");
+
+  c = mongo_sync_connect (config.secondary_host, config.secondary_port,
+			  TRUE);
+  ok (mongo_sync_cmd_is_master (c) == FALSE,
+      "Connected to a secondary");
+
+  ok (mongo_sync_cmd_update (c, config.ns,
+			     MONGO_WIRE_FLAG_UPDATE_UPSERT |
+			     MONGO_WIRE_FLAG_UPDATE_MULTI, sel,
+			     upd) == TRUE,
+      "mongo_sync_cmd_update() automatically reconnects to master");
+  mongo_sync_disconnect (c);
+  endskip;
+
+  bson_free (sel);
+  bson_free (upd);
+  end_network_tests ();
 }
 
-RUN_TEST (5, mongo_sync_cmd_update);
+RUN_TEST (9, mongo_sync_cmd_update);
--- tests/unit/mongo/sync/sync_reconnect.c
+++ tests/unit/mongo/sync/sync_reconnect.c
@@ -2,11 +2,13 @@
 #include "mongo.h"
 
 #include <errno.h>
+#include <sys/socket.h>
+#include "libmongo-private.h"
 
 void
 test_mongo_sync_reconnect (void)
 {
-  mongo_sync_connection *conn;
+  mongo_sync_connection *conn, *o;
 
   ok (mongo_sync_reconnect (NULL, FALSE) == NULL,
       "mongo_sync_reconnect() fails with a NULL connection");
@@ -20,6 +22,105 @@ test_mongo_sync_reconnect (void)
 	  "errno is EHOSTUNREACH");
 
   mongo_sync_disconnect (conn);
+
+  begin_network_tests (15);
+
+  /* Connect & reconnect to master */
+  o = conn = mongo_sync_connect (config.primary_host,
+				 config.primary_port, TRUE);
+  ok ((conn = mongo_sync_reconnect (conn, TRUE)) != NULL,
+      "mongo_sync_reconnect() works when reconnecting to self");
+  ok (o == conn,
+      "Reconnect to an existing master results in the same object");
+  mongo_sync_disconnect (conn);
+
+  /* Connect to master, kill FD, reconnect */
+  conn = mongo_sync_connect (config.primary_host,
+			     config.primary_port, TRUE);
+  mongo_sync_cmd_is_master (conn);
+
+  shutdown (conn->super.fd, SHUT_RDWR);
+  sleep (3);
+
+  ok ((conn = mongo_sync_reconnect (conn, TRUE)) != NULL,
+       "mongo_sync_reconnect() succeed when the connection drops");
+  mongo_sync_disconnect (conn);
+
+  /* Connect, kill, reconnect; w/o knowing other hosts */
+  o = conn = mongo_sync_connect (config.primary_host,
+				 config.primary_port, TRUE);
+  shutdown (conn->super.fd, SHUT_RDWR);
+  sleep (3);
+  conn = mongo_sync_reconnect (conn, FALSE);
+
+  ok (conn != o && conn == NULL,
+      "mongo_sync_reconnect() fails if it can't reconnect anywhere");
+  mongo_sync_disconnect (o);
+
+  /* Gracefully ignore unparsable hosts during reconnect */
+  o = conn = mongo_sync_connect (config.primary_host,
+				 config.primary_port, TRUE);
+  mongo_sync_cmd_is_master (conn);
+  conn->rs.hosts = g_list_prepend (conn->rs.hosts,
+				   g_strdup ("invalid:-42"));
+  shutdown (conn->super.fd, SHUT_RDWR);
+  sleep (3);
+  conn = mongo_sync_reconnect (conn, TRUE);
+
+  ok (conn == o,
+      "mongo_sync_reconnect() gracefully ignores unparsable hosts "
+      "during reconnect");
+  mongo_sync_disconnect (conn);
+
+  /* Ignore unreachable hosts during reconnect */
+  o = conn = mongo_sync_connect (config.primary_host,
+				 config.primary_port, TRUE);
+  mongo_sync_cmd_is_master (conn);
+  conn->rs.hosts = g_list_prepend (conn->rs.hosts,
+				   g_strdup ("example.com:27017"));
+  shutdown (conn->super.fd, SHUT_RDWR);
+  sleep (3);
+  conn = mongo_sync_reconnect (conn, TRUE);
+
+  ok (conn == o,
+      "mongo_sync_reconnect() gracefully ignores unparsable hosts "
+      "during reconnect");
+  mongo_sync_disconnect (conn);
+
+  /*
+   * Tests involving a secondary
+   */
+
+  skip (!config.secondary_host, 9,
+	"Secondary host not set up");
+
+  /* Connect to secondary & reconnect to master */
+  o = conn = mongo_sync_connect (config.secondary_host,
+				 config.secondary_port, TRUE);
+  ok (conn != NULL, "Connecting to secondary");
+  ok (mongo_sync_cmd_is_master (conn) == FALSE,
+      "Connected to a secondary");
+  ok ((conn = mongo_sync_reconnect (conn, TRUE)) != NULL,
+      "Reconnecting from slave to master succeeds");
+  ok (conn == o, "Connection object updated in-place");
+  ok (mongo_sync_cmd_is_master (conn),
+      "Correctly reconnected to master");
+  mongo_sync_disconnect (conn);
+
+  /* Connect to secondary & reconnect to self */
+  o = conn = mongo_sync_connect (config.secondary_host,
+				 config.secondary_port, TRUE);
+  ok (conn != NULL, "Connecting to secondary");
+  ok ((conn = mongo_sync_reconnect (conn, FALSE)) != NULL,
+      "Reconnecting from slave to self succeeds");
+  ok (conn == o, "Connection object updated in-place");
+  ok (mongo_sync_cmd_is_master (conn) == FALSE,
+      "Correctly reconnected to self");
+  mongo_sync_disconnect (conn);
+
+  endskip;
+
+  end_network_tests ();
 }
 
-RUN_TEST (4, mongo_sync_reconnect);
+RUN_TEST (19, mongo_sync_reconnect);
