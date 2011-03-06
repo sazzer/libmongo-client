47fccaf98c148f72f05141d67af2c563c61d723d
Gergely Nagy <algernon@balabit.hu>
2011-03-06 01:21:54 +0100

    tests: Blew up the old mongo-client test into smaller pieces.
    The smaller pieces test specific funcionality, and do not try to
    verify the result we get from the server: the purpose of the
    mongo-client layer is to send and receive, interpretation is the job
    of a higher layer.
    
    Therefore, the new, split tests are considerably smaller than the old
    suite, yet, they test more functionality due to their focus on doing
    just that.

---
+++ tests/func/mongo/client/f_mongo_packet_send.c
@@ -0,0 +1,38 @@
+#include "test.h"
+#include "mongo-client.h"
+#include "mongo-wire.h"
+#include "bson.h"
+
+#include "libmongo-private.h"
+
+#include <sys/socket.h>
+
+void
+test_f_mongo_packet_send (void)
+{
+  mongo_connection *conn;
+  mongo_packet *p;
+  bson *b;
+
+  b = bson_new ();
+  bson_append_int32 (b, "getnonce", 1);
+  bson_finish (b);
+
+  p = mongo_wire_cmd_custom (42, config.db, 0, b);
+  bson_free (b);
+
+  conn = mongo_connect (config.primary_host, config.primary_port);
+  ok (mongo_packet_send (conn, p),
+      "mongo_packet_send() works");
+
+  shutdown (conn->fd, SHUT_RDWR);
+  sleep (1);
+
+  ok (mongo_packet_send (conn, p) == FALSE,
+      "mongo_packet_send() fails on a closed socket");
+  mongo_wire_packet_free (p);
+
+  mongo_disconnect (conn);
+}
+
+RUN_NET_TEST (2, f_mongo_packet_send);
