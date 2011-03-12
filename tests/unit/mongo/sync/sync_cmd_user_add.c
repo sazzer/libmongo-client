7e6ef6516b738ec1a6b84e75da49f016b92b4860
Gergely Nagy <algernon@balabit.hu>
2011-03-12 13:07:11 +0100

    tests: Add networked unit tests for the authentication commands.

--- tests/unit/mongo/sync/sync_cmd_user_add.c
+++ tests/unit/mongo/sync/sync_cmd_user_add.c
@@ -3,19 +3,64 @@
 #include "config.h"
 
 #include <errno.h>
+#include <sys/socket.h>
+#include "libmongo-private.h"
 
 #if ENABLE_AUTH
+void
+test_mongo_sync_cmd_user_add_net_secondary (void)
+{
+  mongo_sync_connection *c;
+  gboolean ret;
+
+  skip (!config.secondary_host, 1,
+	"Secondary server not configured");
+
+  c = mongo_sync_connect (config.secondary_host, config.secondary_port, TRUE);
+
+  ret = mongo_sync_cmd_user_add (c, config.db, "test", "s3kr1+");
+  ok (ret && mongo_sync_cmd_is_master (c),
+      "mongo_sync_cmd_user_add() automatically reconnects to master");
+
+  mongo_sync_disconnect (c);
+
+  endskip;
+}
+
+void
+test_mongo_sync_cmd_user_add_net (void)
+{
+  mongo_sync_connection *c;
+
+  begin_network_tests (3);
+
+  c = mongo_sync_connect (config.primary_host, config.primary_port, TRUE);
+
+  ok (mongo_sync_cmd_user_add (c, config.db, "test", "s3kr1+") == TRUE,
+      "mongo_sync_cmd_user_add() works");
+
+  shutdown (c->super.fd, SHUT_RDWR);
+  sleep (3);
+
+  ok (mongo_sync_cmd_user_add (c, config.db, "test", "s3kr1+") == TRUE,
+      "mongo_sync_cmd_user_add() automatically reconnects");
+
+  mongo_sync_disconnect (c);
+
+  test_mongo_sync_cmd_user_add_net_secondary ();
+
+  end_network_tests ();
+}
+
 void
 test_mongo_sync_cmd_user_add (void)
 {
   mongo_sync_connection *c;
 
-  test_env_setup ();
-
   c = test_make_fake_sync_conn (-1, FALSE);
 
   errno = 0;
-  ok (mongo_sync_cmd_user_add (NULL, config.db, "test", "s3kr1+") == FALSE,
+  ok (mongo_sync_cmd_user_add (NULL, "test", "test", "s3kr1+") == FALSE,
       "mongo_sync_cmd_user_add() fails with a NULL connection");
   cmp_ok (errno, "==", ENOTCONN,
 	  "errno is set to ENOTCONN");
@@ -27,25 +72,26 @@ test_mongo_sync_cmd_user_add (void)
 	  "errno is set to EINVAL");
 
   errno = 0;
-  ok (mongo_sync_cmd_user_add (c, config.db, NULL, "s3kr1+") == FALSE,
+  ok (mongo_sync_cmd_user_add (c, "test", NULL, "s3kr1+") == FALSE,
       "mongo_sync_cmd_user_add() fails with a NULL user");
   cmp_ok (errno, "==", EINVAL,
 	  "errno is set to EINVAL");
 
   errno = 0;
-  ok (mongo_sync_cmd_user_add (c, config.db, "test", NULL) == FALSE,
+  ok (mongo_sync_cmd_user_add (c, "test", "test", NULL) == FALSE,
       "mongo_sync_cmd_user_add() fails with a NULL password");
   cmp_ok (errno, "==", EINVAL,
 	  "errno is set to EINVAL");
 
-  ok (mongo_sync_cmd_user_add (c, config.db, "test", "s3kr1+") == FALSE,
+  ok (mongo_sync_cmd_user_add (c, "test", "test", "s3kr1+") == FALSE,
       "mongo_sync_cmd_user_add() fails with a bogus FD");
 
   mongo_sync_disconnect (c);
-  test_env_free ();
+
+  test_mongo_sync_cmd_user_add_net ();
 }
 
-RUN_TEST (9, mongo_sync_cmd_user_add);
+RUN_TEST (12, mongo_sync_cmd_user_add);
 #else
 void
 test_mongo_sync_cmd_user_add  (void)
--- tests/unit/mongo/sync/sync_cmd_user_remove.c
+++ tests/unit/mongo/sync/sync_cmd_user_remove.c
@@ -3,19 +3,67 @@
 #include "config.h"
 
 #include <errno.h>
+#include <sys/socket.h>
+#include "libmongo-private.h"
 
 #if ENABLE_AUTH
+void
+test_mongo_sync_cmd_user_remove_net_secondary (void)
+{
+  mongo_sync_connection *c;
+  gboolean ret;
+
+  skip (!config.secondary_host, 1,
+	"Secondary server not configured");
+
+  c = mongo_sync_connect (config.secondary_host, config.secondary_port, TRUE);
+
+  mongo_sync_cmd_user_add (c, config.db, "test", "s3kr1+");
+  ret = mongo_sync_cmd_user_remove (c, config.db, "test");
+  ok (ret && mongo_sync_cmd_is_master (c),
+      "mongo_sync_cmd_user_remove() automatically reconnects to master");
+
+  mongo_sync_disconnect (c);
+
+  endskip;
+}
+
+void
+test_mongo_sync_cmd_user_remove_net (void)
+{
+  mongo_sync_connection *c;
+
+  begin_network_tests (3);
+
+  c = mongo_sync_connect (config.primary_host, config.primary_port, TRUE);
+
+  mongo_sync_cmd_user_add (c, config.db, "test", "s3kr1+");
+  ok (mongo_sync_cmd_user_remove (c, config.db, "test") == TRUE,
+      "mongo_sync_cmd_user_remove() works");
+
+  mongo_sync_cmd_user_add (c, config.db, "test", "s3kr1+");
+  shutdown (c->super.fd, SHUT_RDWR);
+  sleep (3);
+
+  ok (mongo_sync_cmd_user_remove (c, config.db, "test") == TRUE,
+      "mongo_sync_cmd_user_remove() automatically reconnects");
+
+  mongo_sync_disconnect (c);
+
+  test_mongo_sync_cmd_user_remove_net_secondary ();
+
+  end_network_tests ();
+}
+
 void
 test_mongo_sync_cmd_user_remove (void)
 {
   mongo_sync_connection *c;
 
-  test_env_setup ();
-
   c = test_make_fake_sync_conn (-1, FALSE);
 
   errno = 0;
-  ok (mongo_sync_cmd_user_remove (NULL, config.db, "test") == FALSE,
+  ok (mongo_sync_cmd_user_remove (NULL, "test", "test") == FALSE,
       "mongo_sync_cmd_user_remove() fails with a NULL connection");
   cmp_ok (errno, "==", ENOTCONN,
 	  "errno is set to ENOTCONN");
@@ -27,19 +75,20 @@ test_mongo_sync_cmd_user_remove (void)
 	  "errno is set to EINVAL");
 
   errno = 0;
-  ok (mongo_sync_cmd_user_remove (c, config.db, NULL) == FALSE,
+  ok (mongo_sync_cmd_user_remove (c, "test", NULL) == FALSE,
       "mongo_sync_cmd_user_remove() fails with a NULL user");
   cmp_ok (errno, "==", EINVAL,
 	  "errno is set to EINVAL");
 
-  ok (mongo_sync_cmd_user_remove (c, config.db, "test") == FALSE,
+  ok (mongo_sync_cmd_user_remove (c, "test", "test") == FALSE,
       "mongo_sync_cmd_user_remove() fails with a bogus FD");
 
   mongo_sync_disconnect (c);
-  test_env_free ();
+
+  test_mongo_sync_cmd_user_remove_net ();
 }
 
-RUN_TEST (7, mongo_sync_cmd_user_remove);
+RUN_TEST (10, mongo_sync_cmd_user_remove);
 #else
 void
 test_mongo_sync_cmd_user_remove  (void)
