b4cdda17a184b7f9bc571def564c6de2f7f2e033
Gergely Nagy <algernon@balabit.hu>
2011-03-12 11:13:31 +0100

    tests: Fix the _sync_cmd_delete() unit test.
    Use "test.ns" instead of the uninitialized config.ns (we use config.ns
    only for the networked tests).

--- tests/unit/mongo/sync/sync_cmd_delete.c
+++ tests/unit/mongo/sync/sync_cmd_delete.c
@@ -93,14 +93,14 @@ test_mongo_sync_cmd_delete (void)
   c = test_make_fake_sync_conn (-1, FALSE);
   b = test_bson_generate_full ();
 
-  ok (mongo_sync_cmd_delete (NULL, config.ns, 0, b) == FALSE,
+  ok (mongo_sync_cmd_delete (NULL, "test.ns", 0, b) == FALSE,
       "mongo_sync_cmd_delete() fails with a NULL connection");
   ok (mongo_sync_cmd_delete (c, NULL, 0, b) == FALSE,
       "mongo_sync_cmd_delete() fails with a NULL namespace");
-  ok (mongo_sync_cmd_delete (c, config.ns, 0, NULL) == FALSE,
+  ok (mongo_sync_cmd_delete (c, "test.ns", 0, NULL) == FALSE,
       "mongo_sync_cmd_delete() fails with a NULL selector");
 
-  ok (mongo_sync_cmd_delete (c, config.ns, 0, b) == FALSE,
+  ok (mongo_sync_cmd_delete (c, "test.ns", 0, b) == FALSE,
       "mongo_sync_cmd_delete() fails with a bogus FD");
 
   bson_free (b);
